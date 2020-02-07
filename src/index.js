a08c2075fe2285fbe3b7536fd3f43b0d6ad5227e
chunyukuo88 <alexgochenour@gmail.com>
2020-02-06 23:21:10 -0500

    Initial setup. npm start works.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('howzit howzit');
+let howzit = document.createElement('h1');
+howzit.innerText = 'howzit';
+howzit.id = 'howzit';
+
+let app = document.getElementById('app');
+app.append(howzit);
