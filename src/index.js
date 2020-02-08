3cee7ea1183d00ec0e94aedfc8334da835578269
chunyukuo88 <alexgochenour@gmail.com>
2020-02-07 19:40:08 -0500

    This is a reference commit, containing content that will be wholly replaced for the actual implementation of the site.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-console.log('howzit howzit');
-let howzit = document.createElement('h1');
-howzit.innerText = 'howzit';
-howzit.id = 'howzit';
-
-let app = document.getElementById('app');
-app.append(howzit);
