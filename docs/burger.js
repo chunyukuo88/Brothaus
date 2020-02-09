c9f0c5ade28dbc0a56a40f012f52c30d6e6210aa
chunyukuo88 <alexgochenour@gmail.com>
2020-02-09 12:24:09 -0500

    Made use of Clippy to style shapes. Applied a hover effect for the captions.

--- docs/burger.js
+++ docs/burger.js
@@ -1,16 +1,15 @@
-function myFunction() {
+function showOrHideMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+    if (!event.target.matches('.drop-button')) {
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
-        }
       }
     }
   }
+}
