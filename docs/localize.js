37966134e7247c4c24f1d6790bd6fde533951123
chunyukuo88 <alexgochenour@gmail.com>
2020-02-12 12:13:15 -0500

    Now the temperature switches to Celsius when you switch to Chinese localization.

--- docs/localize.js
+++ docs/localize.js
@@ -17,12 +17,17 @@ function changeLanguage(){
       about.innerText = '關於';
     const repo = document.getElementById('repo');
       repo.innerText = '本站之原碼';    
-    const language = document.getElementById('chinese');
+//[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
+    const localWeather = document.getElementById('columbus-weather');
+      const currentTemp = document.getElementById('cbus-temp');
+      localWeather.innerText = '這裡有' + fahrenheitToCelsius(currentTemp.innerText) + '°';
+//[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
+      const language = document.getElementById('chinese');
       language.innerText = 'English';  
     const snackbar = document.getElementById('snackbar');
       snackbar.innerText = '本站使用普通JS、Sass以及Web Components等技術。';  
     const jokeInstructions = document.getElementsByClassName('joke-instructions');
-      jokeInstructions[0].innerText = '想學一個無比冷的英文笑話麼？';
+      jokeInstructions[0].innerText = '想學個無比冷的英文笑話麼？';
     const jokeLabel = document.getElementsByClassName('joke-label');
       jokeLabel[0].innerText = '哎，這麼爛的笑話：';
     applyChineseFonts();
@@ -34,7 +39,10 @@ function changeLanguage(){
     const about = document.getElementById('about');
       about.innerText = 'About';
     const repo = document.getElementById('repo');
-      repo.innerText = 'Code';    
+      repo.innerText = 'Code';
+    const localWeather = document.getElementById('columbus-weather');
+      const currentTemp = document.getElementById('cbus-temp');
+      localWeather.innerText = 'It\'s ' + celsiusToFahrenheit(currentTemp) + '° here';    
     const language = document.getElementById('chinese');
       language.innerText = '話語版本';    
     const snackbar = document.getElementById('snackbar');
@@ -54,4 +62,12 @@ function changeLanguage(){
   function applyEnglishFonts(){
     const wooblersHouse = document.getElementById('wooblers-house');
     wooblersHouse.style.fontFamily = 'Luckiest Guy';
+  }
+
+  function fahrenheitToCelsius(degreesF){
+    return Math.round(5/9 * (degreesF-32));
+  }
+
+  function celsiusToFahrenheit(degreesC){
+    return Math.round((degreesC * 1.8) + 32);
   }
