d8da2809f2e4a64f66674f5bbc9251e45a80031f
chunyukuo88 <alexgochenour@gmail.com>
2020-02-21 11:53:13 -0500

    Removed unused files; tweaked cubic bezier on <h1> element.

--- docs/burger.js
+++ docs/burger.js
@@ -4,13 +4,14 @@ function showOrHideMenu() {
 
 window.onclick = function(event) {
   
-  if (!event.target.matches('.drop-button')) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+    if (!event.target.matches('.drop-button')) {
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
-  }
-}
+
 }
