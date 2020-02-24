c9ca04bd92eac6ec6d4539309993cfe7d493d89e
chunyukuo88 <alexgochenour@gmail.com>
2020-02-23 22:05:27 -0500

    Moved the CSS into its own file. The trick is to use <link rel="stylesheet" media="screen" href="./css/loader.css"> in the head of the HTML file, note the middle of that tag. Moved the JavaScript for the loader into its own file, as well.

--- docs/foto.js
+++ docs/foto.js
@@ -20,14 +20,12 @@ class myFoto extends HTMLElement {
       if (this.hasAttribute('img')) {
         imgUrl = this.getAttribute('img');
       } else {
-        imgUrl = 'images/default.png';
+        imgUrl = 'images/z_default.png';
       }
       image.src = imgUrl;
   
-        // Apply external styles to the shadow dom 
       const linkElem = document.createElement('link');
-      // linkElem.setAttribute('rel', 'stylesheet');
-      // linkElem.setAttribute('href', './css/main.css');
+
       let style = document.createElement('style');
       style.textContent = 
       `
@@ -144,8 +142,8 @@ class myFoto extends HTMLElement {
       li .image-wrapper figure img {
         height: 48rem;
         width: 48rem;
-        -webkit-clip-path: polygon(0 0, 9% 7%, 15% 0%, 22% 8%, 32% 0, 100% 0, 100% 100%, 92% 93%, 88% 100%, 79% 93%, 72% 100%, 0 100%);
-        clip-path: polygon(0 0, 9% 7%, 15% 0%, 22% 8%, 32% 0, 100% 0, 100% 100%, 92% 93%, 88% 100%, 79% 93%, 72% 100%, 0 100%);
+        -webkit-clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%);
+        clip-path: polygon(0% 0%, 0% 0%, 0% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%);
       }
       
       li .image-wrapper figure figcaption {
