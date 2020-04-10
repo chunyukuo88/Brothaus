d4cef12bd9fcaa147a4ee950ab228e2a46357ead
chunyukuo88 <alexgochenour@gmail.com>
2020-04-09 23:19:33 -0400

    Refactored copyJoke.js, tweaked styling.

--- docs/copyJoke.js
+++ docs/copyJoke.js
@@ -1,19 +1,28 @@
 function copyToClipboard() {
-  
+  createTextElement();
+  displayJokeSnackbar();
+  removeTextElement();
+};
+
+function createTextElement(){
   const textArea = document.createElement('textarea');
   textArea.setAttribute('style','width:1px;border:0;opacity:0;');
+  textArea.id = 'toBeCopied';
   document.body.appendChild(textArea);
   textArea.value = document.getElementById("joke-content").innerHTML;
   textArea.select();
   document.execCommand('copy');
-  jokeSnackbar();
-  document.body.removeChild(textArea)
 };
 
-function jokeSnackbar(){
+function displayJokeSnackbar(){
   const snackbarMessage = document.getElementById('joke-snackbar');
   snackbarMessage.className = 'visible'
   setTimeout(() => { 
     snackbarMessage.className = snackbarMessage.className.replace('visible', '')
     }, 3000);
+};
+
+function removeTextElement(){
+  const textArea = document.getElementById('toBeCopied');
+  document.body.removeChild(textArea);
 };
