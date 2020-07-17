075d89be184ad0c5dbd3b63c64aa01cfe5beb159
chunyukuo88 <alexgochenour@gmail.com>
2020-07-16 22:49:07 -0400

    Added more testing dependencies.

--- brothaus-react/src/components/App.js
+++ brothaus-react/src/components/App.js
@@ -2,4 +2,8 @@ import React from 'react';
 import Heading from './Heading';
 import Foto from './Foto';
 
-export default () => <><Heading/><Foto/></>;
+export default () =>
+    <div data-test="component-app">
+        <Heading/>
+        <Foto/>
+    </div>;
