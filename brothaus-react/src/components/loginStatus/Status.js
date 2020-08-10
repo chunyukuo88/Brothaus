df9b4487f2829ea78a8c994e307bbbac7e32721d
chunyukuo88 <alexgochenour@gmail.com>
2020-08-09 21:59:32 -0400

    The Status component now reloads on logout.

--- brothaus-react/src/components/loginStatus/Status.js
+++ brothaus-react/src/components/loginStatus/Status.js
@@ -1,40 +1,39 @@
-import React, { useState, useContext, useEffect } from 'react';
-import '../../style/Status.css';
+import React, { useContext, useEffect } from 'react';
 import { AccountContext } from '../../contexts/account-context';
 import { AuthContext }    from '../../contexts/authentication-context';
 import LoginAndSignup from "../LoginAndSignup";
 
-const Status = () => {
-    const [status, setStatus] = useState(false);
-    const [ authStatus, setAuthStatus ] = useContext(AuthContext);
+function Status () {
+    const [ status, setStatus ] = useContext(AuthContext);
     const { getSession, logout } = useContext(AccountContext);
+
+    const logOutAndSetStatusToFalse = () => {
+        setStatus({isAuthenticated: false});
+        logout();
+    }
+
+    const displayLogOutButton = () => {
+        return (
+            <>
+                You are logged in.
+                <button onClick={ logOutAndSetStatusToFalse }>Logout</button>
+            </>
+        );
+    }
 
     useEffect(() => {
         getSession()
             .then(() => {
                 setStatus(true);
             });
-    }, [authStatus, getSession]);
+    }, [status, getSession]);
 
-    return <>{showLoginLogoutFields(status, logout)}</>;
+    return (
+        <>
+            {status === true ? displayLogOutButton(logout): <LoginAndSignup/>}
+        </>
+    );
 };
 
-const showLoginLogoutFields = (status, logout) => {
-    return status === true
-        ? displayLogOutButton(logout)
-        : <LoginAndSignup/>;
-}
-
-const displayLogOutButton = logout => (
-        <>
-            You are logged in.
-            <button onClick={ logoutAndUpdateAuthStatus }>Logout</button>
-        </>
-);
-
-const logoutAndUpdateAuthStatus = (logout, setAuthStatus) => {
-    logout();
-    setAuthStatus(false);
-}
 
 export default Status;
