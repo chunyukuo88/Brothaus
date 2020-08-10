c6717d0f168b951b1eec701795d6e335c95445dc
chunyukuo88 <alexgochenour@gmail.com>
2020-08-09 22:42:20 -0400

    Alignment and spacing issues.

--- brothaus-react/src/components/FotoPrivate.js
+++ brothaus-react/src/components/FotoPrivate.js
@@ -3,8 +3,10 @@ import React from "react";
 export default () => (
         <div>
             <div>Members-only</div>
-            <img src="http://placekitten.com/g/350/350" alt="" />
-            <br/>
-            <img src="http://placekitten.com/g/350/350" alt="" />
+            <div className="foto-wrapper">
+                <img src="http://placekitten.com/g/350/350" alt="" />
+                <br/>
+                <img src="http://placekitten.com/g/350/350" alt="" />
+            </div>
         </div>
     );
