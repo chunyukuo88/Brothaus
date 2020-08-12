f6dd9e3a1737b0def391714af7909ae584fdbfe4
chunyukuo88 <alexgochenour@gmail.com>
2020-08-11 23:18:00 -0400

    WIP: Setup for api calls for weather info.

--- brothaus-react/src/components/DropdownMenuItems.js
+++ brothaus-react/src/components/DropdownMenuItems.js
@@ -1,15 +1,33 @@
 import Settings from "./loginStatus/Settings";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../style/DropdownMenuItems.css';
 import urls from '../urls';
-import WeatherDisplay from "./WeatherDisplay";
+import axios from 'axios';
 
 export default () => {
-        const [English, setEnglish] = useState(true);
+        const [ English, setEnglish ] = useState(true);
+        const [ weather, setWeather ] = useState('Weather');
 
-        const _toggleLanguage = () => {
-          setEnglish(!English);
-        };
+        const getWeatherFromApi = async () => {
+          const apiKey = 'eb366c82727f387afc53658766e245e8';
+          const url = 'https://api.openweathermap.org/data/2.5/weather?q=Columbus,Ohio&appid=' + apiKey;
+          return await axios.get(url);
+        }
+
+        useEffect(() => {
+            getWeatherFromApi();
+        },[]);
+
+        const _toggleWeatherDisplay = () => {
+          if (weather === 'Weather')
+            setWeather('meow');
+          else
+            setWeather('Weather');
+        }
+
+        // const _toggleLanguage = () => {
+        //   setEnglish(!English);
+        // };
 
         const repo = urls.repo;
 
@@ -18,32 +36,32 @@ export default () => {
                  className='mdc-drawer mdc-drawer--dismissible demo-drawer mdc-drawer--modal mdc-drawer--open'
                  data-test='menu-items'
                  data-testid='menu-items'>
-                <Settings />
-                <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'>
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">help</i>
-                        <span className="mdc-list-item__text">About</span>
-                </div>
-                <a className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'
-                   id="repo" href={{repo}} target="_blank" rel="noopener noreferrer">
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">code</i>
-                        <span className="mdc-list-item__text">Code</span>
-                </a>
+                {/*<Settings />*/}
+                {/*<div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'>*/}
+                {/*        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">help</i>*/}
+                {/*        <span className="mdc-list-item__text">About</span>*/}
+                {/*</div>*/}
+                {/*<a className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'*/}
+                {/*   id="repo" href={{repo}} target="_blank" rel="noopener noreferrer">*/}
+                {/*        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">code</i>*/}
+                {/*        <span className="mdc-list-item__text">Code</span>*/}
+                {/*</a>*/}
                 <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'
                    id="columbus-weather"
-                   // onClick={_toggleWeatherDisplay}
+                   onClick={_toggleWeatherDisplay}
                   >
                         <i className="material-icons mdc-list-item__graphic" aria-hidden="true">cloud</i>
                         <span className="mdc-list-item__text">
-                                <WeatherDisplay />
+                                {weather}
                         </span>
                 </div>
-                <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'
-                   id="chinese-or-english"
-                   data-test='language-display'
-                   onClick={_toggleLanguage}>
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">language</i>
-                        <span className="mdc-list-item__text">{English ? 'English' : '華語版本'}</span>
-                </div>
+                {/*<div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'*/}
+                {/*   id="chinese-or-english"*/}
+                {/*   data-test='language-display'*/}
+                {/*   onClick={_toggleLanguage}>*/}
+                {/*        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">language</i>*/}
+                {/*        <span className="mdc-list-item__text">{English ? 'English' : '華語版本'}</span>*/}
+                {/*</div>*/}
             </div>
         );
 };
--- brothaus-react/src/components/WeatherDisplay.js
+++ brothaus-react/src/components/WeatherDisplay.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+
+
 
 function WeatherDisplay(){
   const [ weatherDisplay, setWeatherDisplay ] = useState('Weather');
+
+  // let axiosedWeather = getWeatherFromApi();
+  let axiosedWeather = 'mork';
 
   return (
     <div>
@@ -9,7 +14,12 @@ function WeatherDisplay(){
     </div>
   );
 
+}
 
+export const getWeatherFromApi = async () => {
+  const apiKey = 'eb366c82727f387afc53658766e245e8';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Columbus,Ohio&appid=' + apiKey;
+  return await axios.get(url);
 }
 
 export default WeatherDisplay;
