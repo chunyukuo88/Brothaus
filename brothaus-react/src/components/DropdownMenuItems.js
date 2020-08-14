f6d5f94971ff963ed807fd21b8c6a1769fb6bc78
chunyukuo88 <alexgochenour@gmail.com>
2020-08-14 00:34:07 -0400

    Checked over, adjusted the JSX in DropdownMenuItems.

--- brothaus-react/src/components/DropdownMenuItems.js
+++ brothaus-react/src/components/DropdownMenuItems.js
@@ -1,8 +1,8 @@
-import Settings from "./loginStatus/Settings";
 import React, { useState } from "react";
-import '../style/DropdownMenuItems.css';
-import WeatherDisplay from "./WeatherDisplay";
 import urls from '../urls';
+import Settings from "./loginStatus/Settings";
+import WeatherDisplay from "./WeatherDisplay";
+import '../style/DropdownMenuItems.css';
 
 export default function DropDownMenuItems () {
         const [ English, setEnglish ] = useState(true);
@@ -12,37 +12,26 @@ export default function DropDownMenuItems () {
         };
 
         return (
-            <div id='menu-items'
-                 className='mdc-drawer mdc-drawer--dismissible demo-drawer mdc-drawer--modal mdc-drawer--open'
-                 data-test='menu-items'
-                 data-testid='menu-items'>
-                <Settings />
-                <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'>
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">help</i>
-                        <span className="mdc-list-item__text">About</span>
-                </div>
-                <a className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'
-                   id="repo"
-                   href={urls.githubRepo}
-                   target="_blank">
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">code</i>
-                        <span className="mdc-list-item__text">Code</span>
+            <div id='menu-items' className='mdc-drawer mdc-drawer--modal mdc-drawer--open' data-test='menu-items'>
+                {/*<Settings />*/}
+              <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'>
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">help</i>
+                <span className="mdc-list-item__text" role="menuitem">About</span>
+              </div>
+                <a href={urls.githubRepo} id="repo" className='mdc-list-item mdc-list-item--selected demo-drawer-list-item' target="_blank">
+                  <i className="material-icons mdc-list-item__graphic" aria-hidden="true">code</i>
+                  <span className="mdc-list-item__text">Code</span>
                 </a>
-                <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'
-                   id="columbus-weather"
-                  >
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">cloud</i>
-                        <span className="mdc-list-item__text">
-                                <WeatherDisplay/>
-                        </span>
-                </div>
-                <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item'
-                   id="chinese-or-english"
-                   data-test='language-display'
-                   onClick={_toggleLanguage}>
-                        <i className="material-icons mdc-list-item__graphic" aria-hidden="true">language</i>
-                        <span className="mdc-list-item__text">{English ? 'English' : '華語版本'}</span>
-                </div>
+              <div className='mdc-list-item mdc-list-item--selected demo-drawer-list-item' id="columbus-weather">
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">cloud</i>
+                <span className="mdc-list-item__text">
+                  <WeatherDisplay/>
+                </span>
+              </div>
+              <div onClick={_toggleLanguage} id="chinese-or-english" className='mdc-list-item mdc-list-item--selected demo-drawer-list-item' data-test='language-display'>
+                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">language</i>
+                <span className="mdc-list-item__text">{English ? 'English' : '華語版本'}</span>
+              </div>
             </div>
         );
 };
