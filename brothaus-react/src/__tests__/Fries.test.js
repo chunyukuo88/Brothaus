8cc1e01c05b142c472af7f7f098bc1a39ecd2346
chunyukuo88 <alexgochenour@gmail.com>
2020-08-21 22:58:14 -0400

    Refactored tests. It is possible to extract the render(<Hamburger/>); but not the constants.

--- brothaus-react/src/__tests__/Fries.test.js
+++ brothaus-react/src/__tests__/Fries.test.js
@@ -6,21 +6,22 @@ import userEvent from '@testing-library/user-event';
 import Fries from '../components/Fries';
 import LoginAndSignup from '../components/LoginAndSignup';
 
+beforeEach(()=>{
+  render(<Fries/>);
+});
+
 describe('Fries.js, ', ()=>{
   describe('On load, ', ()=>{
     test('the Fries component renders without crashing,', ()=>{
-      render(<Fries/>);
       expect(screen.getByTestId('fries')).toBeInTheDocument();
     });
     test('and the drawer is closed.', ()=>{
-      render(<Fries/>);
       const drawer = screen.getByTestId('fries-drawer');
       expect(drawer.innerHTML).toBe('');
     });
   });
   describe('When I click the fries icon,', ()=>{
     test('the menu toggle is invoked.', ()=>{
-      render(<Fries/>);
       const drawer = screen.getByTestId('fries-drawer');
       const button = screen.getByTestId('fries-button');
 
--- brothaus-react/src/__tests__/Hamburger.test.js
+++ brothaus-react/src/__tests__/Hamburger.test.js
@@ -5,29 +5,28 @@ import userEvent from '@testing-library/user-event';
 
 import Hamburger from "../components/Hamburger";
 
+beforeEach(()=>{
+    render(<Hamburger/>);
+});
+
 describe('Hamburger.js,', ()=>{
     describe('On load,', ()=>{
        test('the Hamburger component renders without crashing,', ()=>{
-           render(<Hamburger/>);
            const hamburger = screen.getByTestId('hamburger');
            expect(hamburger).toBeInTheDocument();
        });
        test('and the drawer is closed.', ()=>{
-           render(<Hamburger/>);
            const drawer = screen.getByTestId('drawer');
            expect(drawer.innerHTML).toBe('');
        });
     });
     describe('When I click the hamburger icon,', ()=>{
         test('the menu toggle is invoked.', ()=>{
-            render(<Hamburger/>);
             const hamburger = screen.getByTestId('hamburger');
             const drawer = screen.getByTestId('drawer');
 
-            console.log('Before click', drawer.innerHTML);
             expect(drawer.innerHTML).toBe('');
             userEvent.click(hamburger);
-            console.log('After click', drawer.innerHTML);
             expect(drawer.innerHTML).not.toBe('');
         });
     });
