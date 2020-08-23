d31ad8974b27a144bd4e24dc73452f7571d145c5
chunyukuo88 <alexgochenour@gmail.com>
2020-08-23 13:28:30 -0400

    Adjusted foto sizing with image upload script in mind.

--- brothaus-react/src/components/Foto.js
+++ brothaus-react/src/components/Foto.js
@@ -4,25 +4,17 @@ import '../css/FotoPublic.css';
 export default function Foto(){
       return (
         <div className="container">
-            <img title="Pic 0"  src="https://woobler-photos.s3.amazonaws.com/tiger.png" alt=""/>
-            <img title="Pic 1"  src="https://placekeanu.com/500/320/y" alt=""/>
-            <img title="Pic 2"  src="https://placekeanu.com/500/500/y" alt=""/>
-            <img title="Pic 3"  src="https://placekeanu.com/500/500/y" alt=""/>
-            <img title="Pic 4"  src="https://placekeanu.com/500/390/y" alt=""/>
-            <img title="Pic 5"  src="https://placekeanu.com/500/630/y" alt=""/>
-            <img title="Pic 6"  src="https://placekeanu.com/500/570/y" alt=""/>
-            <img title="Pic 7"  src="https://placekeanu.com/500/300/y" alt=""/>
-            <img title="Pic 8"  src="https://placekeanu.com/500/300/y" alt=""/>
-            <img title="Pic 9"  src="https://placekeanu.com/500/300/y" alt=""/>
-            <img title="Pic 10" src="https://placekeanu.com/500/300/y" alt=""/>
-            <img title="Pic 11" src="https://placekeanu.com/500/400/y" alt=""/>
-            <img title="Pic 12" src="https://placekeanu.com/500/450/y" alt=""/>
-            <img title="Pic 13" src="https://placekeanu.com/500/250/y" alt=""/>
-            <img title="Pic 14" src="https://placekeanu.com/500/250/y" alt=""/>
-            <img title="Pic 15" src="https://placekeanu.com/500/150/y" alt=""/>
-            <img title="Pic 16" src="https://placekeanu.com/500/240/y" alt=""/>
-            <img title="Pic 17" src="https://placekeanu.com/500/100/y" alt=""/>
-            <img title="Pic 18" src="https://placekeanu.com/500/100/y" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/0.png" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/1-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/2-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/3-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/4-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/5-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/6-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/7-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/8-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/9-min.jpg" alt=""/>
+            <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/10-min.jpg" alt=""/>
         </div>
       );
 }
