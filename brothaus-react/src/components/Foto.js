b7dffd3f6ad4f88ff1e53ad98daf84b7413d3e8a
chunyukuo88 <alexgochenour@gmail.com>
2020-08-24 19:03:08 -0400

    Removed the annoying title hover tag thing from images.

--- brothaus-react/src/components/Foto.js
+++ brothaus-react/src/components/Foto.js
@@ -5,67 +5,67 @@ export default function Foto(){
       return (
         <div className="container">
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/0.png" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/0.png" alt=""/>
               <figcaption className='figcaption'>
                 Now I try to run at least 12 miles every Saturday or Sunday.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/1-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/1-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae eius labore molestias nulla quaerat, quam ullam voluptas! Dolor, qui.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/2-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/2-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/3-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/3-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, omnis!
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/4-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/4-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus aliquid atque deleniti perferendis. Cum perferendis porro quae quam sapiente.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/5-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/5-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error modi nesciunt repellendus voluptatum? Amet assumenda culpa maxime non ut?
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/6-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/6-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet corporis deleniti dolore laboriosam molestias qui quisquam repudiandae sed sint.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/7-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/7-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, asperiores cum deleniti dolorum eaque exercitationem expedita fugit laboriosam nam nesciunt!
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/8-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/8-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eos fugit id libero non nulla, quis velit voluptas? Amet, optio.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/9-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/9-min.jpg" alt=""/>
               <figcaption className='figcaption'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, exercitationem, quis. A asperiores delectus eius placeat! Commodi explicabo modi recusandae.
               </figcaption>
             </figure>
             <figure>
-              <img title="Pic 1"  src="https://woobler-photos.s3.amazonaws.com/10-min.jpg" alt=""/>
+              <img src="https://woobler-photos.s3.amazonaws.com/10-min.jpg" alt=""/>
               <figcaption className='figcaption'></figcaption>
             </figure>
         </div>
