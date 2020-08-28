8f2fcb3593d140a50b90bc46df6ed911102ac6f8
chunyukuo88 <alexgochenour@gmail.com>
2020-08-28 00:08:29 -0400

    More improvements to the modal.

--- brothaus-react/src/components/AboutModal.js
+++ brothaus-react/src/components/AboutModal.js
@@ -3,15 +3,6 @@ import '../css/AboutModal.css';
 import urls from "../urls";
 
 export default function AboutModal ({ close }){
-  const enzymeLink = () => <a href={urls.enzyme}
-                              target='_blank'
-                              rel='noopener'
-                              className='technology-link'>Enzyme</a>;
-  const reactTesting = () => <a href={urls.reactTestingLibrary}
-                                target='_blank'
-                                rel='noopener'
-                                className='technology-link'>React Testing Library</a>;
-
   return (
     <div className="modal">
       <a className="close" onClick={close} href="www.wooblers-house.xyz">
@@ -20,15 +11,24 @@ export default function AboutModal ({ close }){
       <div className="modal-header">...to my trilingual photo album.</div>
       <div className="content">
         This is a test-driven React project.
-        <br />
-        Modals are provided by <a class='technology-link'
-                                  href='https://www.npmjs.com/package/reactjs-popup'
-                                  target='_blank'
-                                  rel='noopener'>reactjs-popup library</a>.
-        <br />
+        <hr />
+        Modals are provided by {reactPopup()}.
+        <hr />
         100% coverage with {enzymeLink()} & {reactTesting()}.
       </div>
     </div>
   );
 };
 
+const enzymeLink = () => <a href={urls.enzyme}
+                            target='_blank'
+                            rel='noopener'
+                            className='technology-link'>Enzyme</a>;
+const reactTesting = () => <a href={urls.reactTestingLibrary}
+                              target='_blank'
+                              rel='noopener'
+                              className='technology-link'>React Testing Library</a>;
+const reactPopup = () => <a className='technology-link'
+                              href='https://www.npmjs.com/package/reactjs-popup'
+                              target='_blank'
+                              rel='noopener'>reactjs-popup library</a>;
