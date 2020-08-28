fa7581ba373ff42d12c02c23c49f7e21da4d9d53
chunyukuo88 <alexgochenour@gmail.com>
2020-08-27 23:59:27 -0400

    Refactored technology links.

--- brothaus-react/src/components/AboutModal.js
+++ brothaus-react/src/components/AboutModal.js
@@ -1,7 +1,17 @@
 import React from "react";
 import '../css/AboutModal.css';
+import urls from "../urls";
 
 export default function AboutModal ({ close }){
+  const enzymeLink = () => <a href={urls.enzyme}
+                              target='_blank'
+                              rel='noopener'
+                              className='technology-link'>Enzyme</a>;
+  const reactTesting = () => <a href={urls.reactTestingLibrary}
+                                target='_blank'
+                                rel='noopener'
+                                className='technology-link'>React Testing Library</a>;
+
   return (
     <div className="modal">
       <a className="close" onClick={close} href="www.wooblers-house.xyz">
@@ -14,10 +24,11 @@ export default function AboutModal ({ close }){
         Modals are provided by <a class='technology-link'
                                   href='https://www.npmjs.com/package/reactjs-popup'
                                   target='_blank'
-                                  rel="noopener">reactjs-popup library</a>.
+                                  rel='noopener'>reactjs-popup library</a>.
         <br />
-        100% coverage with <a class='technology-link'>Enzyme</a> & React Testing Library.
+        100% coverage with {enzymeLink()} & {reactTesting()}.
       </div>
     </div>
   );
 };
+
