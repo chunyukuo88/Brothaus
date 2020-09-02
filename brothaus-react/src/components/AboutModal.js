04dc81009589602b66211c7fa150ead5e4589e8b
chunyukuo88 <alexgochenour@gmail.com>
2020-09-02 18:04:55 -0400

    Fixed font size and centering for modal.

--- brothaus-react/src/components/AboutModal.js
+++ brothaus-react/src/components/AboutModal.js
@@ -2,10 +2,9 @@ import React from 'react';
 import '../css/AboutModal.css';
 import urls from '../urls';
 
-export default function AboutModal ({ close }){
+export default function AboutModal (){
   return (
     <div className='modal'>
-
       <div className='modal-header'>...to my trilingual photo album.</div>
       <div className='content'>
         This is a test-driven React project.
