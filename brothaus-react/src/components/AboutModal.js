6725c1078d0f2b7548f05d8cca3f8427d2cb12e2
chunyukuo88 <alexgochenour@gmail.com>
2020-09-02 17:55:09 -0400

    Various fixes to modal and foto components. WIP: Trying to figure out optimal approach for captions.

--- brothaus-react/src/components/AboutModal.js
+++ brothaus-react/src/components/AboutModal.js
@@ -1,34 +1,32 @@
-import React from "react";
+import React from 'react';
 import '../css/AboutModal.css';
-import urls from "../urls";
+import urls from '../urls';
 
 export default function AboutModal ({ close }){
   return (
-    <div className="modal">
-      <a className="close" onClick={close} href="www.wooblers-house.xyz">
-        ×
-      </a>
-      <div className="modal-header">...to my trilingual photo album.</div>
-      <div className="content">
+    <div className='modal'>
+
+      <div className='modal-header'>...to my trilingual photo album.</div>
+      <div className='content'>
         This is a test-driven React project.
-        <hr />
-        Modals are provided by {reactPopup()}.
-        <hr />
-        100% coverage with {enzymeLink()} & {reactTesting()}.
+        <hr/>
+        Modals are provided by {reactPopup}.
+        <hr/>
+        100% coverage with {enzymeLink} & {reactTesting}.
       </div>
     </div>
   );
 };
 
-const enzymeLink = () => <a href={urls.enzyme}
-                            target='_blank'
-                            rel='noopener'
-                            className='technology-link'>Enzyme</a>;
-const reactTesting = () => <a href={urls.reactTestingLibrary}
-                              target='_blank'
-                              rel='noopener'
-                              className='technology-link'>React Testing Library</a>;
-const reactPopup = () => <a className='technology-link'
-                              href='https://www.npmjs.com/package/reactjs-popup'
-                              target='_blank'
-                              rel='noopener'>reactjs-popup library</a>;
+const enzymeLink = <a href={urls.enzyme}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='technology-link'>Enzyme</a>;
+const reactTesting = <a href={urls.reactTestingLibrary}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='technology-link'>React Testing Library</a>;
+const reactPopup = <a className='technology-link'
+                      href='https://www.npmjs.com/package/reactjs-popup'
+                      target='_blank'
+                      rel='noopener noreferrer'>reactjs-popup library</a>;
