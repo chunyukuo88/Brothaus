114aa290e90bb6dba20c74e17722666e83e6e787
chunyukuo88 <alexgochenour@gmail.com>
2020-09-08 22:50:14 -0400

    WIP: Links are turning up as objects when passed into the getter functions of AboutModal.

--- brothaus-react/src/components/AboutModal.js
+++ brothaus-react/src/components/AboutModal.js
@@ -4,29 +4,63 @@ import urls from '../urls';
 import { connect } from 'react-redux';
 
 class AboutModal extends Component {
+
+  enzymeLink = <a href={urls.enzyme}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='technology-link'>Enzyme</a>;
+  reactTesting = <a href={urls.reactTestingLibrary}
+                          target='_blank'
+                          rel='noopener noreferrer'
+                          className='technology-link'>React Testing Library</a>;
+  reactPopup = () => <a className='technology-link'
+                        href='https://www.npmjs.com/package/reactjs-popup'
+                        target='_blank'
+                        rel='noopener noreferrer'>reactjs-popup library</a>;
   render(){
     return (
       <div className='modal'>
         <div className='modal-header'>{this._getModalHeader(this.props)}</div>
         <div className='content'>
-          This is a test-driven React project.
+          <p className='summary-text'>{this._getSummaryText(this.props, )}</p>
           <hr/>
-          Modals are provided by {reactPopup}.
+          <p className='modal-text'>{this._getModalDescription(this.props, this.reactPopup)}</p>
           <hr/>
-          100% coverage with {enzymeLink} & {reactTesting}.
+          <p className='statement'>{this._getCoverageStatement(this.props, this.enzymeLink, this.reactTesting)}</p>
         </div>
       </div>
     );
   };
-
   _getModalHeader = ({language}) => {
     switch (language){
-      case 'chinese': return '歡迎使用小巫之屋～';
-      case 'english': return '...to my trilingual photo album.';
+      case 'chinese': return '歡迎訪問小巫之屋～';
       case 'russian': return '... в мой трехъязычный фотоальбом.';
-      default: return 'Source Code';
+      default: return '...to my trilingual photo album.';
     }
   };
+  _getSummaryText = ({language}) => {
+    switch (language){
+      case 'chinese': return '製作本站，我用了REACT與REDUX';
+      case 'russian': return 'Это проект React.';
+      default: return 'This is a test-driven React project.';
+    }
+  }
+  _getModalDescription = ({language}, reactPopup) => {
+    switch (language){
+      case 'chinese': return `我用了${reactPopup}來拼湊出這個模態框`;
+      case 'russian': return `Модальные блоки предоставлены библиотекой ${reactPopup()}`;
+      default: return `Modals are provided by ${reactPopup}`;
+    }
+  };
+
+  _getCoverageStatement = ({language}, enzymeLink, reactTesting) => {
+    switch (language){
+      case 'chinese': return `為了達成百分百測試覆蓋率，我用了${enzymeLink}及${reactTesting}。`;
+      case 'russian': return `100% тестового покрытия с библиотекой ${enzymeLink} и ${reactTesting}.`;
+      default: return `100% test coverage with ${enzymeLink} & ${reactTesting}.`;
+    }
+  };
+
 }
 
 function mapStateToProps(state){
@@ -36,15 +70,4 @@ export default connect(mapStateToProps)(AboutModal);
 
 
 
-const enzymeLink = <a href={urls.enzyme}
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      className='technology-link'>Enzyme</a>;
-const reactTesting = <a href={urls.reactTestingLibrary}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className='technology-link'>React Testing Library</a>;
-const reactPopup = <a className='technology-link'
-                      href='https://www.npmjs.com/package/reactjs-popup'
-                      target='_blank'
-                      rel='noopener noreferrer'>reactjs-popup library</a>;
+
