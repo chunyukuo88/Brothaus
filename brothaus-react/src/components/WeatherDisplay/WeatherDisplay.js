139dd9acaa06ec296b4b8895466e7810884b99f1
chunyukuo88 <alexgochenour@gmail.com>
2020-09-10 18:22:43 -0400

    Fixed the useSelector import. I had been importing it from 'react' rather than 'react-redux'. Next: Fix the degree types for Russian and English.

--- brothaus-react/src/components/WeatherDisplay/WeatherDisplay.js
+++ brothaus-react/src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import urls from '../../urls';
+import { useSelector } from 'react-redux';
 import {ChineseWeatherDisplay, EnglishWeatherDisplay, RussianWeatherDisplay} from './WeatherDisplayLocalizations';
 
 export default function WeatherDisplay (props) {
   const [ degreesKelvin, setDegreesKelvin] = useState(270);
   const [ humidity, setHumidity ] = useState(50);
+  const selectedLanguage = useSelector((state) => state.language);
 
-  const display = (props.isFahrenheit)
-    ? getEnglishDisplay(getDegreesFahrenheit(degreesKelvin), humidity)
-    : getRussianDisplay(getDegreesCelsius(degreesKelvin), humidity);
+  const displayLanguage = (language) => {
+    switch(language){
+      case 'russian': return getRussianDisplay(getDegreesFahrenheit(degreesKelvin), humidity);
+      case 'chinese': return getChineseDisplay(getDegreesCelsius(degreesKelvin), humidity);
+      default:        return getEnglishDisplay(getDegreesCelsius(degreesKelvin), humidity);
+    }
+  }
 
   const getWeatherFromApi = async () => {
     const result = await fetch(urls.openWeatherUrl).then(res => res.json());
@@ -20,7 +26,7 @@ export default function WeatherDisplay (props) {
     getWeatherFromApi();
   },[]);
 
-  return <>{display}</>;
+  return <>{displayLanguage(selectedLanguage)}</>;
 }
 
 const getDegreesFahrenheit = degreesKelvin => (9/5) * (degreesKelvin - 273) + 32;
