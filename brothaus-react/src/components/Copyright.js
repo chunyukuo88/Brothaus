2cb09bdef6079e428d0b962a6a856c5477e8bfaa
chunyukuo88 <alexgochenour@gmail.com>
2020-09-10 22:52:46 -0400

    Improved visual effects of titles.

--- brothaus-react/src/components/Copyright.js
+++ brothaus-react/src/components/Copyright.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Copyright(){
-  return <div id='copyright'>Copyright Alex Gochenour 2020</div>;
+  return <div id='copyright' className='english'>Copyright Alex Gochenour 2020</div>;
 }
