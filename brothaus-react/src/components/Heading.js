b7a3e78d0da13c7d1e7885f339145fa559495d8d
chunyukuo88 <alexgochenour@gmail.com>
2020-09-11 21:05:22 -0400

    Refactored Heading.js

--- brothaus-react/src/components/Heading.js
+++ brothaus-react/src/components/Heading.js
@@ -20,24 +20,30 @@ export default function Heading(){
     <div data-testid='heading' id='heading'>
       <Title/>
       <nav id='nav-items'>
-        <div id='nav-items__welcome' className='nav-item'>
-              <Welcome/>
-        </div>
-        <div id='nav-items__code' className='nav-item'>
-              <Code/>
-        </div>
+        {getWelcomeString()}
+        {getCodeString()}
+        {getLocalizationStrings(dispatch)}
         <div id='nav-items__weather'
              className='nav-item'
              onClick={_toggleDegrees}
              data-test='weather-display'>
               <WeatherDisplay {...degrees}/>
         </div>
-        <div id='nav-items__language' className='nav-item'>
-              <span id='rus' className='localization russian' onClick={()=>dispatch(switchToRussian())}>🇷🇺</span>
-              <span id='ch' className='localization chinese' onClick={()=>dispatch(switchToChinese())}>🇹🇼</span>
-              <span id='en' className='localization english' onClick={()=>dispatch(switchToEnglish())}>🇺🇸</span>
-        </div>
       </nav>
     </div>
   );
 };
+
+const getWelcomeString = () => <div id='nav-items__welcome' className='nav-item'><Welcome/></div>;
+
+const getCodeString = () => <div id='nav-items__code' className='nav-item'><Code/></div>;
+
+const getLocalizationStrings = (dispatch) => {
+  return (
+    <div id='nav-items__language' className='nav-item'>
+      <span id='rus' className='localization russian' onClick={()=>dispatch(switchToRussian())}>🇷🇺</span>
+      <span id='en' className='localization english' onClick={()=>dispatch(switchToEnglish())}>🇺🇸</span>
+      <span id='ch' className='localization chinese' onClick={()=>dispatch(switchToChinese())}>🇹🇼</span>
+    </div>
+  );
+}
