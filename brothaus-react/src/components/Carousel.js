9d9498d250293af0b1ceeab62dcf08b4f67c1dd3
chunyukuo88 <alexgochenour@gmail.com>
2020-09-16 23:38:40 -0400

    Breakthrough! Finally found some up-to-date documentation for the s3.listObjects() method. Next: Work with the object to populate carousel with the number gained from data.Contents from the bucket object.

--- brothaus-react/src/components/Carousel.js
+++ brothaus-react/src/components/Carousel.js
@@ -3,13 +3,29 @@ import '../css/FotoPublic.css';
 import Foto from './Foto';
 import AWS from 'aws-sdk';
 
+//TODO: Finish the tutorial:
+//https://medium.com/javascript-in-plain-english/using-node-js-to-display-images-in-a-private-aws-s3-bucket-4c043ed5c5d0
+
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:1f7f72b9-a2e9-4c6e-a304-08eae3599fb5',
+});
+
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const albumBucketName = 'woobler-photos';
+const bucketParams = {Bucket: albumBucketName};
+
+
+
 export default function Carousel(){
-  const fotoAlbum = 'woobler-photos';
-  const s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    params: { Bucket: fotoAlbum}
-  })
-  console.log(s3.listObjects());
+  s3.listObjects(bucketParams, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.Contents);
+    }
+  });
+
   const foto1 = {fotoNumber: 1};
   const foto2 = {fotoNumber: 2};
   const foto3 = {fotoNumber: 3};
