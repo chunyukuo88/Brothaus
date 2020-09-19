1bc07798b422dfb72bb01dec9924c7807c64069f
chunyukuo88 <alexgochenour@gmail.com>
2020-09-19 01:33:25 -0400

    WIP: This is the general direction I'm moving towards to dynamically populate the carousel. This code does not work at the moment.

--- brothaus-react/src/components/Carousel/Carousel.js
+++ brothaus-react/src/components/Carousel/Carousel.js
@@ -1,42 +1,27 @@
 import React from 'react';
 import '../../css/FotoPublic.css';
 import Foto from '../Foto';
-import getPhotoCount from './photoCount';
+import getFotoCount from './photoCount';
 
 export default function Carousel(){
 
-  let photoCount;
-  getPhotoCount()
+  let carousel = <div className="carousel"
+                      data-flickity='{ "lazyLoad": true,
+                                       "freeScroll": true,
+                                       "dragThreshold": 6,
+                                       "wrapAround": true }'>
+                 </div>;
+
+  getFotoCount()
     .then(populateCarousel);
 
   function populateCarousel(data) {
-    photoCount = data;
-    console.log('photoCount: ', photoCount);
+    while (data > 0) {
+      const props = {fotoNumber: data};
+      carousel.appendChild(<Foto {...props}/>);
+      data--;
+    }
   }
 
-
-  const foto1 = {fotoNumber: 1};
-  const foto2 = {fotoNumber: 2};
-  const foto3 = {fotoNumber: 3};
-  const foto4 = {fotoNumber: 4};
-  const foto5 = {fotoNumber: 5};
-  const foto6 = {fotoNumber: 6};
-  const foto7 = {fotoNumber: 7};
-  const foto8 = {fotoNumber: 8};
-  return(
-    <div className="carousel"
-         data-flickity='{ "lazyLoad": true,
-                          "freeScroll": true,
-                          "dragThreshold": 6,
-                          "wrapAround": true }'>
-      <Foto {...foto1}/>
-      <Foto {...foto2}/>
-      <Foto {...foto3}/>
-      <Foto {...foto4}/>
-      <Foto {...foto5}/>
-      <Foto {...foto6}/>
-      <Foto {...foto7}/>
-      <Foto {...foto8}/>
-    </div>
-  );
+  return({carousel});
 }
--- brothaus-react/src/components/Carousel/photoCount.js
+++ brothaus-react/src/components/Carousel/photoCount.js
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 
-AWS.config.region = 'us-east-1'; // Region
+AWS.config.region = 'us-east-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: 'us-east-1:1f7f72b9-a2e9-4c6e-a304-08eae3599fb5',
 });
@@ -9,7 +9,7 @@ const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const albumBucketName = 'woobler-photos';
 const bucketParams = {Bucket: albumBucketName};
 
-export default async function getPhotoCount() {
+export default async function getFotoCount() {
   const result = await s3.listObjectsV2(bucketParams).promise()
     .then(data => data.Contents.length);
   return result;
