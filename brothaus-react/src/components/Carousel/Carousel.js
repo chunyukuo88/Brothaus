5a7ef8abf12c2afae9e16f038cc51897d3dfbbc7
chunyukuo88 <alexgochenour@gmail.com>
2020-09-19 16:31:12 -0400

    The populateCarousel() needed a key to render each <Foto/> component properly. Next: Have it iterate based on the number of photos in the S3 bucket.

--- brothaus-react/src/components/Carousel/Carousel.js
+++ brothaus-react/src/components/Carousel/Carousel.js
@@ -5,23 +5,28 @@ import getFotoCount from './photoCount';
 
 export default function Carousel(){
 
-  let carousel = <div className="carousel"
-                      data-flickity='{ "lazyLoad": true,
-                                       "freeScroll": true,
-                                       "dragThreshold": 6,
-                                       "wrapAround": true }'>
-                 </div>;
+  // return <div>
+  //   {
+  //     [1,2,3].map ( (n) => {
+  //       return this.renderSquare(n)
+  //     })
+  //   }
+  // </div>;
 
   getFotoCount()
     .then(populateCarousel);
 
-  function populateCarousel(data) {
-    while (data > 0) {
-      const props = {fotoNumber: data};
-      carousel.appendChild(<Foto {...props}/>);
-      data--;
-    }
+  function populateCarousel() {
+      return [1,2,3,4,5,6,7,8].map(number => <Foto key={number} {...{fotoNumber: number}}/>);
   }
 
-  return({carousel});
+  return(
+      <div className="carousel"
+          data-flickity='{ "lazyLoad": true,
+                           "freeScroll": true,
+                           "dragThreshold": 6,
+                           "wrapAround": true }'>
+        {populateCarousel()}
+     </div>
+  );
 }
--- brothaus-react/src/components/Foto.js
+++ brothaus-react/src/components/Foto.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Foto(props){
   const fotoPath = `https://woobler-photos.s3.amazonaws.com/${props.fotoNumber}-min.jpg`;
   return (
-    <div className="carousel__cell">
+    <div className="carousel__cell" key={props.fotoNumber}>
       <img className="carousel__cell__image"
            data-flickity-lazyload={fotoPath} alt=""/>
     </div>
