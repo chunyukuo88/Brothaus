354c79993ecb4fa6f4518a5c4932301d0a8b1dde
chunyukuo88 <alexgochenour@gmail.com>
2020-09-20 23:33:50 -0400

    Unhappy path test for switchLanguage reducer now passes.

--- brothaus-react/src/reducers/switchLanguage.test.js
+++ brothaus-react/src/reducers/switchLanguage.test.js
@@ -1,15 +1,25 @@
 import languageReducer from '../reducers/switchLanguage';
-import { RUSSIAN, CHINESE, English } from '../actions/types';
+import { CHINESE } from '../actions/types';
 
 describe('switchLanguage.js reducer:', ()=>{
-  describe('When invoked, ', ()=>{
-    test('it handles actions of type CHINESE', ()=>{
+  describe('When invoked with a valid action type such as CHINESE, ', ()=>{
+    test('it handles the action and returns "chinese".', ()=>{
       const action = {
         type: CHINESE,
-        payload: 'russian'
+        payload: 'chinese'
       };
-      const newState = languageReducer('english', action);
-      expect(newState).toEqual(action.payload);
+      const newState = languageReducer(undefined, action);
+      expect(newState).toEqual('chinese');
+    });
+    describe('When invoked with an invalid type, ', ()=>{
+      test('it handles the action and returns the default state.', ()=>{
+        const action = {
+          type: 'CRAP',
+          payload: 'crap'
+        };
+        const newState = languageReducer(undefined, action);
+        expect(newState).toEqual('english');
+      });
     });
   });
 });
