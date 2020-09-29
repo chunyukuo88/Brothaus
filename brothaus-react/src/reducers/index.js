1041a3d3474a4b83e9348a5b83ebbb0028987e9a
chunyukuo88 <alexgochenour@gmail.com>
2020-09-28 22:03:20 -0400

    Added missing globalHumidity reducer to reducers/index.js

--- brothaus-react/src/reducers/index.js
+++ brothaus-react/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import languageReducer from './languageReducer';
 import globalTempReducer from './globalTempReducer';
+import globalHumidityReducer from "./globalHumidityReducer";
 
 /*
 * This combines multiple reducers into a single object.
 * */
 export default combineReducers({
   language: languageReducer,
-  globalTemp: globalTempReducer
+  globalTemp: globalTempReducer,
+  globalHumidity: globalHumidityReducer,
 });
