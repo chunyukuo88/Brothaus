9ec4d7fb4bfff0e45fd9f9c177f0aae7fee37434
chunyukuo88 <alexgochenour@gmail.com>
2020-10-13 14:42:25 -0400

    Nixed a lot of the junk that should have been on a separate branch, fixed title styling.

--- brothaus-react/src/components/Title/English.js
+++ brothaus-react/src/components/Title/English.js
@@ -4,7 +4,7 @@ export class EnglishTitle extends Component {
   render() {
     return(
       <div className='title-container'>
-        <div className='title english-title'>Woobler's<br/> House</div>
+        <div className='title english-title'>Woobler's House</div>
       </div>
     );
   }
