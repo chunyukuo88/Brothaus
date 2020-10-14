9d39ae753233e6a660b5180de443928701b6b1dc
chunyukuo88 <alexgochenour@gmail.com>
2020-10-14 09:50:07 -0400

    Added React-ga and used it in a useEffect in App.js

--- brothaus-react/src/components/App/App.js
+++ brothaus-react/src/components/App/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Heading from '../Heading/Heading';
 import Carousel from '../Carousel/Carousel';
 import '../../css/common.css';
 import Copyright from '../Copyright/Copyright';
 import { useDispatch } from 'react-redux';
 import { getGlobalHumidity, getGlobalTemp } from '../../actions/actions';
+import ReactGa from 'react-ga';
 
 export default function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('UA-168681251-1');
+    //Report page views from page root:
+    ReactGa.pageview('/');
+  }, []);
+
   const dispatch = useDispatch();
     return (
       <div id='app'
