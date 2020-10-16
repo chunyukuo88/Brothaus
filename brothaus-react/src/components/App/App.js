5b97a7a41d77c296c0acafc82146fa8da9b8150c
chunyukuo88 <alexgochenour@gmail.com>
2020-10-16 11:08:37 -0400

    Fixed .env after reading the React documentation for environment variables.

--- brothaus-react/src/components/App/App.js
+++ brothaus-react/src/components/App/App.js
@@ -10,8 +10,7 @@ import ReactGa from 'react-ga';
 export default function App() {
 
   useEffect(() => {
-    // ReactGa.initialize(`${process.env.GOOGLE_ANALYTICS_UA}`);
-    ReactGa.initialize(`UA-180643596-1`);
+    ReactGa.initialize(`${process.env.REACT_APP_GA}`);
     ReactGa.pageview(window.location.pathname + window.location.search);
   }, []);
 
