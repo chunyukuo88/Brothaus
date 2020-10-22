4222e4e9ca6f18e038f8b247c5a7d08fc69c1127
chunyukuo88 <alexgochenour@gmail.com>
2020-10-21 23:57:25 -0400

    WIP: Continue tweaking FotoPublic.sass

--- brothaus-react/src/components/Carousel/Carousel.js
+++ brothaus-react/src/components/Carousel/Carousel.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import '../../css/FotoPublic.css';
 import Foto from '../Foto/Foto';
-// import getFotoCount from './photoCount';
+import '../../css/Carousel.css';
 
 export default function Carousel(){
 
-  let fotoCount;
-
-  // getFotoCount()
-  //   .then(result => populateCarousel(result));
-
-  function populateCarousel(data) {
-      // fotoCount = data;
-      // console.log('fotoCount: ', fotoCount);
-      // const fotoCountAsArray = arrayFromFotoCount(fotoCount);
+  function populateCarousel() {
       const fotoCountAsArray = arrayFromFotoCount(8); //TODO: Have this use getFotoCount() or Redux.
       return fotoCountAsArray.map(fotoNumber => <Foto key={fotoNumber} {...{fotoNumber: fotoNumber}}/>);
   }
@@ -33,10 +25,4 @@ function arrayFromFotoCount (int) {
   }
   console.log('array: ', arr);
   return arr;
-  // let result = [];
-  // let i = 1
-  // while(i < int+1){
-  //   result.push(i);
-  // }
-  // return result;
 }
