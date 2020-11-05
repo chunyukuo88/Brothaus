c03ed64795f2c4f2ad060192ceb6c3c2a7a38e3c
chunyukuo88 <alexgochenour@gmail.com>
2020-11-04 23:06:44 -0500

    This carousel library does not do well with custom components.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel as CarouselDisplay } from 'react-responsive-carousel';
-import Foto from '../Foto/Foto';
+import { carouselGA } from "../../googleAnalytics/foto";
 
 export default function Carousel() {
   return (
     <CarouselDisplay>
-      <CarouselContent/>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/1-min.jpg" />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/2-min.jpg" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/3-min.jpg" />
+        <p className="legend">Legend 3</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/4-min.jpg" />
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/5-min.jpg" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/6-min.jpg" />
+        <p className="legend">Legend 3</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/7-min.jpg" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img onClick={carouselGA} alt="" src="https://woobler-photos.s3.amazonaws.com/8-min.jpg" />
+        <p className="legend">Legend 3</p>
+      </div>
     </CarouselDisplay>
   );
 };
 
-const CarouselContent = () => {
-  let arr = [1,2,3,4,5,6,7,8];
-  return arr.map(fotoNumber => <Foto key={fotoNumber} {...{fotoNumber: fotoNumber}}></Foto>)
-};
