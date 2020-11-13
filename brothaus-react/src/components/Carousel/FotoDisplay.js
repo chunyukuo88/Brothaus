785c36b966d93f1a6e29e9a16ebf2018bd90269c
Alex Gochenour <alexgochenour@Alexs-MacBook-Pro.local>
2020-11-12 23:28:42 -0500

    WIP: In disarray, but exploring ways to clean up the previously raw, inefficient html in the JSX earlier.

--- brothaus-react/src/components/Carousel/FotoDisplay.js
+++ brothaus-react/src/components/Carousel/FotoDisplay.js
@@ -2,40 +2,61 @@ import React from 'react';
 import '../../css/carousel.css';
 import { Carousel as CarouselDisplay } from 'react-responsive-carousel';
 
+const captions = {
+    caption1: 'We just did.',
+    caption2: 'Cut mirchi at my favorite restaurant: Neehee\'s in Dublin!',
+    caption3: 'I downloaded this beautiful game to celebrate Halloween with The Fourth.',
+    caption4: 'Mugi\'s seasonal handiwork.',
+    caption5: 'I tried khaman, a savory Gujarati cake.',
+    caption6: 'We are getting better at making fires. More specifically, Mugi is. Sunday\'s roast was very nice indeed, and we used very few briquets.',
+    caption7: 'After procrastinating for more than a year, I got this along with an updated driver\'s license.',
+    caption8: 'Mile-high wochebrot was achieved by using a cast iron griddle as a baking stone. I think iron is much more conductive than stone, so the rise on this loaf was incredible.',
+}
+
+const produceFotoSource = (integer) => `https://woobler-photos.s3.amazonaws.com/${integer}-min.jpg`;
+
+const FirstFot = ({image, caption}) => {
+    return <div>
+        <img alt='' src={image}/>
+        <p className="legend">{caption}</p>
+    </div>
+};
+
 export default function FotoDisplay(){
-  return(
+  const props = {
+    image: produceFotoSource(1),
+    caption: captions.caption1,
+  }
+    return(
     <CarouselDisplay>
+      <FirstFot {...props} />
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/1-min.jpg" />
-        <p className="legend">"We just did."</p>
+        <img alt="" src={produceFotoSource(2)} />
+        <p className="legend">{captions.caption2}</p>
       </div>
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/2-min.jpg" />
-        <p className="legend">Cut mirchi at my favorite restaurant: Neehee's!</p>
+        <img alt="" src={produceFotoSource(3)} />
+        <p className="legend">{captions.caption3}</p>
       </div>
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/3-min.jpg" />
-        <p className="legend">I downloaded this beautiful game to celebrate Halloween with The Fourth.</p>
+        <img alt="" src={produceFotoSource(4)} />
+        <p className="legend">{captions.caption4}</p>
       </div>
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/4-min.jpg" />
-        <p className="legend">Mugi's seasonal handiwork.</p>
+        <img alt="" src={produceFotoSource(5)} />
+        <p className="legend">{captions.caption5}</p>
       </div>
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/5-min.jpg" />
-        <p className="legend">I tried khaman, a savory Gujarati cake.</p>
+        <img alt="" src={produceFotoSource(6)} />
+        <p className="legend">{captions.caption6}</p>
       </div>
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/6-min.jpg" />
-        <p className="legend">We are getting better at making fires. More specifically, Mugi is. Sunday's roast was very nice indeed, and we used very few briquets.</p>
+        <img alt="" src={produceFotoSource(7)} />
+        <p className="legend">{captions.caption7}</p>
       </div>
       <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/7-min.jpg" />
-        <p className="legend">After procrastinating for more than a year, I got this along with an updated driver's license.</p>
-      </div>
-      <div>
-        <img alt="" src="https://woobler-photos.s3.amazonaws.com/8-min.jpg" />
-        <p className="legend">Mile-high wochebrot was achieved by using a cast iron griddle as a baking stone. I think iron is much more conductive than stone, so the rise on this loaf was incredible.</p>
+        <img alt="" src={produceFotoSource(8)} />
+        <p className="legend">{captions.caption8}</p>
       </div>
     </CarouselDisplay>
   );
