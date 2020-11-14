ea8b1bba5c04283f5acf7a105de85b6a9ea06f4e
Alex Gochenour <alexgochenour@Alexs-MacBook-Pro.local>
2020-11-13 19:12:06 -0500

    Improved the carousel.

--- brothaus-react/src/components/Carousel/FotoDisplay.js
+++ brothaus-react/src/components/Carousel/FotoDisplay.js
@@ -1,62 +1,49 @@
 import React from 'react';
 import '../../css/carousel.css';
 import { Carousel as CarouselDisplay } from 'react-responsive-carousel';
+import captions from './captions';
 
-const captions = {
-    caption1: 'We just did.',
-    caption2: 'Cut mirchi at my favorite restaurant: Neehee\'s in Dublin!',
-    caption3: 'I downloaded this beautiful game to celebrate Halloween with The Fourth.',
-    caption4: 'Mugi\'s seasonal handiwork.',
-    caption5: 'I tried khaman, a savory Gujarati cake.',
-    caption6: 'We are getting better at making fires. More specifically, Mugi is. Sunday\'s roast was very nice indeed, and we used very few briquets.',
-    caption7: 'After procrastinating for more than a year, I got this along with an updated driver\'s license.',
-    caption8: 'Mile-high wochebrot was achieved by using a cast iron griddle as a baking stone. I think iron is much more conductive than stone, so the rise on this loaf was incredible.',
-}
+/**
+ * Dynamically populating the contents of <CarouselDisplay> is not possible with the react-responsive-carousel
+ * library. Doing so results in browser console errors and the images show up but the captions bunch up.
+ * */
 
-const produceFotoSource = (integer) => `https://woobler-photos.s3.amazonaws.com/${integer}-min.jpg`;
-
-const FirstFot = ({image, caption}) => {
-    return <div>
-        <img alt='' src={image}/>
-        <p className="legend">{caption}</p>
-    </div>
-};
+const _buildFotoSource = (integer) => `https://woobler-photos.s3.amazonaws.com/${integer}-min.jpg`;
 
 export default function FotoDisplay(){
-  const props = {
-    image: produceFotoSource(1),
-    caption: captions.caption1,
-  }
     return(
     <CarouselDisplay>
-      <FirstFot {...props} />
       <div>
-        <img alt="" src={produceFotoSource(2)} />
-        <p className="legend">{captions.caption2}</p>
+        <img alt="" src={_buildFotoSource(1)} />
+        <p className="legend">{captions[1]}</p>
       </div>
       <div>
-        <img alt="" src={produceFotoSource(3)} />
-        <p className="legend">{captions.caption3}</p>
+        <img alt="" src={_buildFotoSource(2)} />
+        <p className="legend">{captions[2]}</p>
       </div>
       <div>
-        <img alt="" src={produceFotoSource(4)} />
-        <p className="legend">{captions.caption4}</p>
+        <img alt="" src={_buildFotoSource(3)} />
+        <p className="legend">{captions[3]}</p>
       </div>
       <div>
-        <img alt="" src={produceFotoSource(5)} />
-        <p className="legend">{captions.caption5}</p>
+        <img alt="" src={_buildFotoSource(4)} />
+        <p className="legend">{captions[4]}</p>
       </div>
       <div>
-        <img alt="" src={produceFotoSource(6)} />
-        <p className="legend">{captions.caption6}</p>
+        <img alt="" src={_buildFotoSource(5)} />
+        <p className="legend">{captions[5]}</p>
       </div>
       <div>
-        <img alt="" src={produceFotoSource(7)} />
-        <p className="legend">{captions.caption7}</p>
+        <img alt="" src={_buildFotoSource(6)} />
+        <p className="legend">{captions[6]}</p>
       </div>
       <div>
-        <img alt="" src={produceFotoSource(8)} />
-        <p className="legend">{captions.caption8}</p>
+        <img alt="" src={_buildFotoSource(7)} />
+        <p className="legend">{captions[7]}</p>
+      </div>
+      <div>
+        <img alt="" src={_buildFotoSource(8)} />
+        <p className="legend">{captions[8]}</p>
       </div>
     </CarouselDisplay>
   );
---
+++ brothaus-react/src/components/Carousel/captions.js
@@ -0,0 +1,12 @@
+const captions = {
+    1: 'We just did.',
+    2: 'Cut mirchi at my favorite restaurant: Neehee\'s in Dublin!',
+    3: 'I downloaded this beautiful game to celebrate Halloween with The Fourth.',
+    4: 'Mugi\'s seasonal handiwork.',
+    5: 'I tried khaman, a savory Gujarati cake.',
+    6: 'We are getting better at making fires. More specifically, Mugi is. Sunday\'s roast was very nice indeed, and we used very few briquets.',
+    7: 'After procrastinating for more than a year, I got this along with an updated driver\'s license.',
+    8: 'Mile-high wochebrot was achieved by using a cast iron griddle as a baking stone. I think iron is much more conductive than stone, so the rise on this loaf was incredible.',
+}
+
+export default captions;
