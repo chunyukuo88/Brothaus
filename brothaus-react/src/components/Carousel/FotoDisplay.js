653968f411eeabc9bbc22268ea141b8ebbd79349
chunyukuo88 <alexgochenour@gmail.com>
2020-11-19 18:38:40 -0500

    Renamed a function.

--- brothaus-react/src/components/Carousel/FotoDisplay.js
+++ brothaus-react/src/components/Carousel/FotoDisplay.js
@@ -8,41 +8,41 @@ import captions from './captions';
  * library. Doing so results in browser console errors and the images show up but the captions bunch up.
  * */
 
-const _buildFotoSource = (integer) => `https://woobler-photos.s3.amazonaws.com/${integer}-min.jpg`;
+const _getFotoSource = (integer) => `https://woobler-photos.s3.amazonaws.com/${integer}-min.jpg`;
 
 export default function FotoDisplay(){
     return(
     <CarouselDisplay>
       <div>
-        <img alt="" src={_buildFotoSource(1)} />
+        <img alt="" src={_getFotoSource(1)} />
         <p className="legend">{captions[1]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(2)} />
+        <img alt="" src={_getFotoSource(2)} />
         <p className="legend">{captions[2]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(3)} />
+        <img alt="" src={_getFotoSource(3)} />
         <p className="legend">{captions[3]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(4)} />
+        <img alt="" src={_getFotoSource(4)} />
         <p className="legend">{captions[4]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(5)} />
+        <img alt="" src={_getFotoSource(5)} />
         <p className="legend">{captions[5]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(6)} />
+        <img alt="" src={_getFotoSource(6)} />
         <p className="legend">{captions[6]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(7)} />
+        <img alt="" src={_getFotoSource(7)} />
         <p className="legend">{captions[7]}</p>
       </div>
       <div>
-        <img alt="" src={_buildFotoSource(8)} />
+        <img alt="" src={_getFotoSource(8)} />
         <p className="legend">{captions[8]}</p>
       </div>
     </CarouselDisplay>
