d54e5b280d771d3ae339250ba4ae6c6a79648ede
chunyukuo88 <alexgochenour@gmail.com>
2020-11-19 16:28:22 -0500

    Refactoring Code component.

--- brothaus-react/src/components/Code/Code.js
+++ brothaus-react/src/components/Code/Code.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import staticStrings from '../../StaticStrings';
 
-class Code extends Component {
-  render(){
-    return codeString(this.props);
-  };
+const Code = () => {
+  const selectedLang = useSelector((state) => state.language);
+  return codeString(selectedLang);
 }
 
 const codeString = props => <a href={process.env.REACT_APP_GITHUB_REPO}
@@ -14,16 +13,12 @@ const codeString = props => <a href={process.env.REACT_APP_GITHUB_REPO}
                                rel='noopener noreferrer'
                                id='repo'>{getCodeDisplay(props)}</a>;
 
-const getCodeDisplay = props => {
-  switch (props.language){
+const getCodeDisplay = language => {
+  switch (language){
     case 'chinese': return <div className='chinese'>{staticStrings.code.ZH}</div>;
     case 'russian': return <div className='russian'>{staticStrings.code.RU}</div>;
     default:        return <div className='english'>{staticStrings.code.EN}</div>;
   }
 }
 
-function mapStateToProps(state){
-  return { language: state.language };
-}
-
-export default connect(mapStateToProps)(Code);
+export default Code;
--- brothaus-react/src/components/Input/Input.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-export default function Input(props) {
-    return (
-      <div data-test="Input">
-          <input />
-      </div>
-    );
-};
