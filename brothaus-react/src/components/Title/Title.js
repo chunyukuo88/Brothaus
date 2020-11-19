cfeb1932c6b48d378e74253a0c74f76d9a9c9536
chunyukuo88 <alexgochenour@gmail.com>
2020-11-19 00:01:00 -0500

    WIP: Title is broken.

--- brothaus-react/src/components/Title/Title.js
+++ brothaus-react/src/components/Title/Title.js
@@ -1,11 +1,25 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { titleStrings, LocalizedTitle } from './LocalizedTitle';
 import * as actions from '../../actions/actions';
 import { mouseOverTitle } from '../../googleAnalytics/heading';
+import _ from 'lodash';
 import '../../css/Title.css';
 
-class Title extends PureComponent {
+const _buildProps = (language) => {
+  const clonedStrings = _.cloneDeep(titleStrings);
+  return {
+    language: language,
+    localizedString: clonedStrings[language]
+  }
+};
+
+const englishProps = _buildProps('english');
+const russianProps = _buildProps('russian');
+const chineseProps = _buildProps('chinese');
+
+
+class Title extends Component {
   render() {
     return (
       <div onMouseEnter={mouseOverTitle}
@@ -18,23 +32,13 @@ class Title extends PureComponent {
 }
 
 const TitleBasedOnLanguage = ({language}) => {
+  console.log('TitleBasedOnLanguage');
     switch (language){
       case 'chinese': return <LocalizedTitle {...chineseProps}/>;
       case 'russian': return <LocalizedTitle {...russianProps}/>;
       default:        return <LocalizedTitle {...englishProps}/>;
     }
 };
-
-const _buildProps = (language) => {
-  return {
-    language: language,
-    localizedString: titleStrings[language]
-  }
-};
-
-const englishProps = _buildProps('english');
-const russianProps = _buildProps('russian');
-const chineseProps = _buildProps('chinese');
 
 function mapStateToProps(state){
   return {
