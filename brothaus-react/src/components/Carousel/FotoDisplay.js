22760019812d7938c8dea67cd914e05172b5221b
chunyukuo88 <alexgochenour@gmail.com>
2020-12-25 21:13:14 -0500

    Removed the third-party carousel component from FotoDisplay.js. Next: Add CSS modules.

--- brothaus-react/src/components/Carousel/FotoDisplay.js
+++ brothaus-react/src/components/Carousel/FotoDisplay.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import '../../css/carousel.css';
 import urls, { getFotoSource } from '../../urls';
-import { Carousel as CarouselDisplay } from 'react-responsive-carousel';
+import './FotoDisplay.css';
 
 /**
  * Dynamically populating the contents of <CarouselDisplay> is not possible with the react-responsive-carousel
@@ -16,40 +15,12 @@ export default function FotoDisplay(){
   }, []);
 
     return(
-    <CarouselDisplay>
+    <>
       <div>
-        <img alt="" src={getFotoSource(1)} />
-        <p className="legend">{captions[0]}</p>
+        <img className="image" alt="" src={getFotoSource(1)} />
+        <p>{captions[0]}</p>
       </div>
-      <div>
-        <img alt="" src={getFotoSource(2)} />
-        <p className="legend">{captions[1]}</p>
-      </div>
-      <div>
-        <img alt="" src={getFotoSource(3)} />
-        <p className="legend">{captions[2]}</p>
-      </div>
-      <div>
-        <img alt="" src={getFotoSource(4)} />
-        <p className="legend">{captions[3]}</p>
-      </div>
-      <div>
-        <img alt="" src={getFotoSource(5)} />
-        <p className="legend">{captions[4]}</p>
-      </div>
-      <div>
-        <img alt="" src={getFotoSource(6)} />
-        <p className="legend">{captions[5]}</p>
-      </div>
-      <div>
-        <img alt="" src={getFotoSource(7)} />
-        <p className="legend">{captions[6]}</p>
-      </div>
-      <div>
-        <img alt="" src={getFotoSource(8)} />
-        <p className="legend">{captions[7]}</p>
-      </div>
-    </CarouselDisplay>
+    </>
   );
 }
 
