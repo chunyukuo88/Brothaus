4c9124b459131ec241ef7acaf1ef362ab81c722e
chunyukuo88 <alexgochenour@gmail.com>
2021-02-16 00:21:31 -0500

    Fixed test coverage for languageReducer.

--- brothaus-react/src/reducers/languageReducer.test.js
+++ brothaus-react/src/reducers/languageReducer.test.js
@@ -1,5 +1,5 @@
 import languageReducer from './languageReducer';
-import { CHINESE } from '../actions/types';
+import { CHINESE, RUSSIAN, ENGLISH } from '../actions/types';
 
 describe('languageReducer.js reducer:', ()=>{
   describe('When invoked with a valid action type such as CHINESE, ', ()=>{
@@ -10,6 +10,22 @@ describe('languageReducer.js reducer:', ()=>{
       };
       const newState = languageReducer(undefined, action);
       expect(newState).toEqual('chinese');
+    });
+    test('AND: It does the same for english.', ()=>{
+      const action = {
+        type: ENGLISH,
+        payload: 'english'
+      };
+      const newState = languageReducer(undefined, action);
+      expect(newState).toEqual('english');
+    });
+    test('AND: It does the same for russian.', ()=>{
+      const action = {
+        type: RUSSIAN,
+        payload: 'russian'
+      };
+      const newState = languageReducer(undefined, action);
+      expect(newState).toEqual('russian');
     });
     describe('When invoked with an invalid type, ', ()=>{
       test('it handles the action and returns the default state.', ()=>{
