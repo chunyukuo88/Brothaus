f4b17a8609fb024c50caea7322a27871292d9965
Alex Gochenour <alexgochenour@gmail.com>
2021-03-12 12:46:43 -0500

    The rest of the files.

--- fire_up_3000.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd brothaus-react && npm start
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import Root from './Root';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <Root>
+    <App/>
+  </Root>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
